696bf11dbeab75aeaf8519c28bdeb1b4dc95e142
mohamedazizjnayah <mohamedazizjnayah@gmail.com>
2026-01-17 10:50:35 +0100

    Add Predict Vials Service

---
+++ backend-SpringBoot/BabyGuardianBackend/feature-ml-service/src/main/java/com/example/featuremlservice/FeatureMlServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.featuremlservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeatureMlServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeatureMlServiceApplication.class, args);
+    }
+
+}
